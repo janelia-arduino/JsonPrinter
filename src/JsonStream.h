bb3b6c5ad598a2e6dab0ae8f2f9f31366245fadc
Peter Polidoro <peterpolidoro@gmail.com>
2021-09-16 13:58:35 -0400

    Update email address

--- src/JsonStream.h
+++ src/JsonStream.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro peterpolidoro@gmail.com
+// Peter Polidoro peter@polidoro.io
 // ----------------------------------------------------------------------------
 #ifndef _JSON_STREAM_H_
 #define _JSON_STREAM_H_
--- src/JsonStream/JsonStreamDefinitions.h
+++ src/JsonStream/JsonStreamDefinitions.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro peterpolidoro@gmail.com
+// Peter Polidoro peter@polidoro.io
 // ----------------------------------------------------------------------------
 #ifndef _JSON_STREAM_DEFINITIONS_H_
 #define _JSON_STREAM_DEFINITIONS_H_
